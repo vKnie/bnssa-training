b76298026eed3355cc832fd085ffe7b59b101047
ZKSOnDaTrack <Kelyfane@gmail.com>
2025-03-03 15:58:27 +0100

    Ajout de package pour la nav-bar et ajout d'un fichier pour la nav-bar

---
+++ navigation/nav-bar.tsx
@@ -0,0 +1,17 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Accueil" component={HomeScreen} />
+        <Tab.Screen name="Profil" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
